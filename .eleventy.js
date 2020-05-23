570c4e29b4a982b214cc746fbacd5c0fc22f4c81
gboi <giulia93boi@gmail.com>
2020-05-23 12:27:19 +0200

    refactoring .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,13 @@
-module.exports = function(eleventyConfig) {
-	
-  let markdownIt = require("markdown-it");
-  let markdownItEmoji = require("markdown-it-emoji");
-  let options = {
+const htmlmin = require("html-minifier");
+const markdownIt = require("markdown-it");
+const markdownItEmoji = require("markdown-it-emoji");
+const options = {
     html: true
-  };
+};
+
+module.exports = function(eleventyConfig) {
   let markdownLib = markdownIt(options).use(markdownItEmoji);
-  
   eleventyConfig.setLibrary("md", markdownLib);
-	
-	
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('img');
   return {
